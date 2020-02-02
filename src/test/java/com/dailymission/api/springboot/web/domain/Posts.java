a6159bb2d37745638e0651776777c6660450a6b3
Minho Lee <alsgkgk77@gmail.com>
2020-02-02 11:53:16 +0900

    create Posts domain

---
+++ src/test/java/com/dailymission/api/springboot/web/domain/Posts.java
@@ -0,0 +1,35 @@
+package com.dailymission.api.springboot.web.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.*;
+
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
