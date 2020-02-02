800b4599c70152ab8a6bb20b6341a18c6535184e
Minho Lee <alsgkgk77@gmail.com>
2020-02-02 21:58:26 +0900

    add queryDsl dependency & configuration

---
+++ src/main/java/com/dailymission/api/springboot/config/QueryDslConfiguration.java
@@ -0,0 +1,19 @@
+package com.dailymission.api.springboot.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+public class QueryDslConfiguration {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
