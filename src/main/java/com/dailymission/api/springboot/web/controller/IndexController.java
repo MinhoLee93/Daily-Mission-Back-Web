acf1d061eb1b2649ead56a322ecc5c21909f9061
Minho Lee <alsgkgk77@gmail.com>
2020-02-02 21:11:18 +0900

    create post-save mustache

--- src/main/java/com/dailymission/api/springboot/web/controller/IndexController.java
+++ src/main/java/com/dailymission/api/springboot/web/controller/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/posts/save")
+    public String postSave(){
+        return "posts-save";
+    }
 }
