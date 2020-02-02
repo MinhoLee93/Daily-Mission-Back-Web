7ed67af1cea6bf05695a86f7db58aa0203bf15c3
Minho Lee <alsgkgk77@gmail.com>
2020-02-02 21:49:29 +0900

    add all posts lists in index.mustache

--- src/main/java/com/dailymission/api/springboot/web/domain/posts/PostsRepository.java
+++ src/main/java/com/dailymission/api/springboot/web/domain/posts/PostsRepository.java
@@ -3,6 +3,9 @@ package com.dailymission.api.springboot.web.domain.posts;
 import com.dailymission.api.springboot.web.domain.posts.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
+    List<Posts> findAllDesc();
 }
