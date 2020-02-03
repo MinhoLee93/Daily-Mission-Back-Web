dd7f7550775238c67f73bf97d79a2e89a3c6ff7a
Minho Lee <alsgkgk77@gmail.com>
2020-02-03 13:30:13 +0900

    change api controller data type Object -> Json (GSON)

--- src/main/java/com/dailymission/api/springboot/web/controller/PostsApiController.java
+++ src/main/java/com/dailymission/api/springboot/web/controller/PostsApiController.java
@@ -1,15 +1,16 @@
 package com.dailymission.api.springboot.web.controller;
 
-
 import com.dailymission.api.springboot.web.dto.PostsListResponseDto;
-import com.dailymission.api.springboot.web.dto.PostsResponseDto;
 import com.dailymission.api.springboot.web.dto.PostsSaveRequestDto;
 import com.dailymission.api.springboot.web.dto.PostsUpdateRequestDto;
 import com.dailymission.api.springboot.web.service.PostsService;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RequiredArgsConstructor
 @RestController
@@ -18,28 +19,38 @@ public class PostsApiController {
     private final PostsService postsService;
 
     @GetMapping("/api/posts/{id}")
-    public PostsResponseDto findById (@PathVariable Long id){
-        return postsService.findById(id);
+    public String findById (@PathVariable Long id){
+        String json = new Gson().toJson(postsService.findById(id));
+
+        return json;
     }
 
     @PostMapping("/api/posts")
-    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+    public Long save(@RequestBody String requestJson){
+        PostsSaveRequestDto requestDto =  new Gson().fromJson(requestJson, PostsSaveRequestDto.class);
+
         return postsService.save(requestDto);
     }
 
     @PutMapping("/api/posts/{id}")
-    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+    public Long update(@PathVariable Long id, @RequestBody String requestJson){
+        PostsUpdateRequestDto requestDto =  new Gson().fromJson(requestJson, PostsUpdateRequestDto.class);
+
         return postsService.update(id, requestDto);
     }
 
     @DeleteMapping("/api/posts/{id}")
     public Long delete(@PathVariable Long id){
         postsService.delete(id);
+
         return id;
     }
 
     @GetMapping("/api/posts/all")
-    public List<PostsListResponseDto> all(){
-        return postsService.findAllDesc();
+    public String all(){
+        List<PostsListResponseDto> responseDtoList =  postsService.findAllDesc();
+        String json = new Gson().toJson(responseDtoList);
+
+        return json;
     }
 }
