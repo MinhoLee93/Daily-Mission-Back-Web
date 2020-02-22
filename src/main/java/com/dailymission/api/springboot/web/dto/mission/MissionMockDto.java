197dbf99564afc8c6d724683379cb62e1cd227fe
Minho Lee <alsgkgk77@gmail.com>
2020-02-22 14:28:46 +0900

    User 로그인후 참여 미션목록 데이터 변경

--- src/main/java/com/dailymission/api/springboot/web/dto/mission/MissionMockDto.java
+++ src/main/java/com/dailymission/api/springboot/web/dto/mission/MissionMockDto.java
@@ -7,15 +7,11 @@ import lombok.Getter;
 public class MissionMockDto {
     private Long id;
     private String title;
-    private String content;
-    private String imageUrl;
     private boolean banned;
 
     public MissionMockDto(Mission entity, Boolean banned){
         this.id = entity.getId();
         this.title = entity.getTitle();
-        this.content = entity.getContent();
-        this.imageUrl = entity.getImageUrl();
         this.banned = banned;
     }
 }
