c6d83d4e9720428e5f7da44c40c2fa3b7d07c484
Minho Lee <alsgkgk77@gmail.com>
2020-03-24 15:24:00 +0900

    CORS 변경 test1 # 전체허용 -> https://daily-mission.com (CloudFront)

--- src/main/java/com/dailymission/api/springboot/config/WebMvcConfig.java
+++ src/main/java/com/dailymission/api/springboot/config/WebMvcConfig.java
@@ -17,7 +17,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://daily-mission.com")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
