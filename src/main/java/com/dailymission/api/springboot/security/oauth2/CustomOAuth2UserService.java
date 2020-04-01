06df7e42fd78c9140a711971a5f856b2013923d3
Minho Lee <alsgkgk77@gmail.com>
2020-04-01 17:22:35 +0900

    USER Default 이미지 변경

--- src/main/java/com/dailymission/api/springboot/security/oauth2/CustomOAuth2UserService.java
+++ src/main/java/com/dailymission/api/springboot/security/oauth2/CustomOAuth2UserService.java
@@ -29,8 +29,8 @@ import java.util.Optional;
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
-    public static final String DEFAULT_USER_IMAGE_URL = "https://s3.ap-northeast-2.amazonaws.com/image.daily-mission.com/%EA%B8%B0%EB%B3%B8%EC%9D%B4%EB%AF%B8%EC%A7%80/user_default_image.png";
-    public static final String DEFAULT_USER_THUMBNAIL_URL = "https://s3.ap-northeast-2.amazonaws.com/image.daily-mission.com/%EA%B8%B0%EB%B3%B8%EC%9D%B4%EB%AF%B8%EC%A7%80/user_default_thumbnail.png";
+    public static final String DEFAULT_USER_IMAGE_URL = "https://image.daily-mission.com/%EA%B8%B0%EB%B3%B8%EC%9D%B4%EB%AF%B8%EC%A7%80/User_Defualt_Image_150_150.png";
+    public static final String DEFAULT_USER_THUMBNAIL_URL = "https://image.daily-mission.com/%EA%B8%B0%EB%B3%B8%EC%9D%B4%EB%AF%B8%EC%A7%80/User_Default_Image_40_40.png";
 
     @Autowired
     private UserRepository userRepository;
