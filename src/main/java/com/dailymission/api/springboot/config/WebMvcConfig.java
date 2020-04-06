c7b4b0d116ad420e691a16c20670c27a659d325b
Minho Lee <alsgkgk77@gmail.com>
2020-04-06 09:13:15 +0900

    CORS 임시 Open (*)

--- src/main/java/com/dailymission/api/springboot/config/WebMvcConfig.java
+++ src/main/java/com/dailymission/api/springboot/config/WebMvcConfig.java
@@ -17,7 +17,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://daily-mission.com")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
